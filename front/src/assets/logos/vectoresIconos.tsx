6ad0528b65b95b12f359f148a8eb1d5148d0b14f
FranMack <franmack556@gmail.com>
2024-02-16 20:41:00 -0300

    build 1

--- front/src/assets/logos/vectoresIconos.tsx
+++ front/src/assets/logos/vectoresIconos.tsx
@@ -1,6 +1,6 @@
 function OkCircle() {
 	return (
-	  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve">
+	  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xmlSpace="preserve">
 		<g fill="none" stroke="green" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
 		  <circle cx="12" cy="12" r="10" fill="" />
 		  <polygon points="9.8,16.8 6.1,13.2 7.5,11.7 9.8,14 15.5,7.9 17,9.3" fill="white" />
